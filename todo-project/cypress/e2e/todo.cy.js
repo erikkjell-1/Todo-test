b1c6691477dbac8fb683076054fbdd097376a48c
plindstromm <mukkepp@gmail.com>
2023-05-24 15:55:53 +0200

    test add

--- todo-project/cypress/e2e/todo.cy.js
+++ todo-project/cypress/e2e/todo.cy.js
@@ -1,6 +1,11 @@
-describe('template spec', () => {
+describe('Add spec', () => {
   beforeEach(() => {
-    cy.visit('https://example.cypress.io');
+    cy.visit('http://127.0.0.1:5500/todo-project/');
     
+
+    cy.get('todo__text').type('Gör allt du kan')
+    cy.get('todo__name').type('Allan')
+    cy.get('todo__button').click();
+
   })
 });
