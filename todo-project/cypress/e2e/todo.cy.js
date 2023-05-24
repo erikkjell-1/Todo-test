af4a9bb3df2f21cec3df306bf87146e8f7ce914b
plindstromm <mukkepp@gmail.com>
2023-05-24 19:29:42 +0200

    Update todo.cy.js

--- todo-project/cypress/e2e/todo.cy.js
+++ todo-project/cypress/e2e/todo.cy.js
@@ -3,9 +3,9 @@ describe('Add spec', () => {
     cy.visit('http://127.0.0.1:5500/todo-project/');
     
 
-    cy.get('todo__text').type('Gör allt du kan')
-    cy.get('todo__name').type('Allan')
+    cy.get('todo__text').type('Gör allt du kan');
+    cy.get('todo__name').type('Allan');
     cy.get('todo__button').click();
 
-  })
-});
+  });
+})
